d2c6b216ae1cb2bb83843670a9c34fad6c311e4e
Clara Meniz <clarameniz@gmail.com>
2018-03-21 17:22:49 +0100

    WIZZIE-1366: Improve reload interval values on dashboards. (#42)

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,13 @@
 module DashboardsHelper
+  def reload_intervals
+    [
+      ['30s', 30],
+      ['1m',  60],
+      ['5m',  300],
+      ['15m', 900],
+      ['30m', 1800],
+      ['1h',  3600],
+      ['2h',  7200]
+    ]
+  end
 end
