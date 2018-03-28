3675721627fcd83308d3294492fc73217f19a39f
José Antonio Parra <josparbar@gmail.com>
2018-03-28 09:59:59 +0200

    WIZZIE-1374: translate gps coordenates to x-y points.
    * WIZZIE-1373 & WIZZIE-1375: plane widget with an image and heatmap layer.
    
    * The heatmap layer is using test data.
    
    * WIZZIE-1373 & WIZZIE-1375: add react-heatmap package.
    
    * WIZZIE-1374: translate gps coordenates to x-y points.

--- app/javascript/wizz-vis/components/widgets/WidgetPlane.jsx
+++ app/javascript/wizz-vis/components/widgets/WidgetPlane.jsx
@@ -1,41 +1,111 @@
 /*jshint esversion: 6 */
 import React from 'react';
 import ReactHeatmap from '../../vendor/ReactHeatmap';
+import gps_utils from './../../utils/gps';
 
 export default class WidgetPlane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getImgSize = this.getImgSize.bind(this);
+
+    this.state = {
+      $$data: [],
+      aggregator: '',
+      coordinate_dimension: '',
+      img_width: 0,
+      img_height: 0,
+      gps_markers: [],
+      fetchDataError: null
+    };
+  }
 
   componentDidMount() {
+    this.getImgSize();
+    this.setDimensionAggregator();
     this.fetchData();
+  }
+
+  setDimensionAggregator() {
+    let coordinate_dimension =
+      this.props.dimensions.find((e) => (
+        /coordinate|latlong|latlng/.test(e.name)
+      ));
+
+    this.setState({
+      coordinate_dimension: coordinate_dimension.name,
+      aggregator: this.props.aggregators[0].name
+    });
   }
 
   fetchData() {
     let button = $('.preloader-wrapper[widget_id="' + this.props.id + '"]');
     button.addClass('active');
-    button.removeClass('active');
+    return (
+      fetch('/widgets/' + this.props.id + '/data.json')
+        .then(response => response.json())
+        .then(data => data.filter((d) => d[this.state.coordinate_dimension] !== null))
+        .then(
+          data => data.map(function(d) {
+            let latitude = d[this.state.coordinate_dimension].split(',')[0];
+            let longitude = d[this.state.coordinate_dimension].split(',')[1];
+
+            let {x, y} = this.translatePoint(latitude, longitude);
+
+            return {
+              x,
+              y,
+              value: d[this.state.aggregator]
+            };
+          }, this)
+        )
+        .then(data => this.setState({ $$data: data }))
+        .then(data => button.removeClass('active'))
+    );
+  }
+
+  translatePoint(latitude, longitude) {
+    const width = this.state.img_width;
+    const height = this.state.img_height;
+
+    const {m_trans, m_offset} =
+      gps_utils.latlngToPointMatrices(this.props.options.gps_markers,
+                                      width,
+                                      height);
+
+    var {x, y} = gps_utils.latlngToPercent(latitude, longitude, width, height,
+                                          m_trans, m_offset);
+
+    return {x, y};
   }
 
   getImageURL() {
     return this.props.options.image;
   }
 
-  render () {
-    const data = [
-      { x: 9, y: 5, value: 30}, { x: 5, y: 1, value: 5},
-      { x: 8, y: 4, value: 5}, { x: 8, y: 4, value: 5},
-      { x: 8, y: 4, value: 5}, { x: 10, y: 15, value: 5},
-      { x: 50, y: 40, value: 2}, { x: 20, y: 20, value: 3},
-      { x: 50, y: 30, value: 3}, { x: 40, y: 15, value: 3},
-      { x: 50, y: 2, value: 2}, { x: 20, y: 40, value: 2},
-      { x: 10, y: 5, value: 2}, { x: 8, y: 4, value: 5}
-    ];
+  getImgSize() {
+    var newImg = new Image();
+    var self = this;
 
+    newImg.onload = function() {
+      self.setState({
+        img_width: newImg.width,
+        img_height: newImg.height
+      });
+    };
+
+    newImg.src = this.props.options.image;
+  }
+
+  render () {
     return (
       <div style={{ position: 'relative', width: '100%', height: '100%' }}>
         <img style={{ position: 'absolute', top: 0, left: 0,
                       width: '100%', height: '100%' }}
-          src={ this.getImageURL() }
+          src = { this.getImageURL() }
         />
-      <ReactHeatmap max={5} data={data} />
+      <ReactHeatmap
+        data={this.state.$$data}
+      />
       </div>
     )
   }
---
+++ app/javascript/wizz-vis/utils/gps.js
@@ -0,0 +1,80 @@
+/*jshint esversion: 6 */
+var math = require('mathjs');
+
+export default {
+  /**
+   * Obtain the matrices for conversions between latitude-longitude
+   * to x-y.
+   *
+   * @param {Object[]} gps_points
+   * @param {number} gps_points[].x
+   * @param {number} gps_points[].y
+   * @param {number} gps_points[].latitude
+   * @param {number} gps_points[].longitude
+   * @param {number} width
+   * @param {number} height
+   * @returns {Object} transformation and offset matrices {m_trans:, m_offset:}
+   */
+  latlngToPointMatrices(gps_points, width, height) {
+    const matrix = math.matrix(
+      [
+        [gps_points[0].x, gps_points[0].y, 0,        0,        1, 0],
+        [0,        0,        gps_points[0].x, gps_points[0].y, 0, 1],
+        [gps_points[1].x, gps_points[1].y, 0,        0,        1, 0],
+        [0,        0,        gps_points[1].x, gps_points[1].y, 0, 1],
+        [gps_points[2].x, gps_points[2].y, 0,        0,        1, 0],
+        [0,        0,        gps_points[2].x, gps_points[2].y, 0, 1]
+      ]
+    );
+
+    const lat_lng_column = math.matrix(
+      [
+        [gps_points[0].longitude], [gps_points[0].latitude], [gps_points[1].longitude],
+        [gps_points[1].latitude], [gps_points[2].longitude], [gps_points[2].latitude]
+      ]
+    );
+
+    const sol = math.multiply(math.inv(matrix), lat_lng_column);
+
+    const m_trans = math.matrix(
+      [
+        [sol.get([0, 0]), sol.get([1, 0])],
+        [sol.get([2, 0]), sol.get([3, 0])]
+      ]
+    );
+
+    const m_offset = math.matrix(
+      [
+        [sol.get([4, 0])],
+        [sol.get([5, 0])]
+      ]
+    );
+
+    return { m_trans: math.inv(m_trans), m_offset: m_offset };
+  },
+
+  /**
+   * Convert point from latitude-longitude reference to percentages referenced
+   * by a square using matrices for transformation and offset.
+   *
+   * @param {number} latitude
+   * @param {number} longitude
+   * @param {number} width
+   * @param {number} height
+   * @param {Matrix} m_trans
+   * @param {Matrix} m_offset
+   * @returns {Object} x and y entries representing percentage
+   */
+  latlngToPercent(latitude, longitude, width, height, m_trans, m_offset) {
+    var points =
+      math.multiply(m_trans,
+                    math.subtract(math.matrix([[longitude], [latitude]]),
+                                  m_offset)
+      );
+
+    return {
+      x: points.get([0, 0]) / width * 100,
+      y: points.get([1, 0]) / height * 100
+    };
+  }
+};
