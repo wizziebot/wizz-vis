806dbe395c8bf3be256d9d166f690e691c5587bb
Clara Meniz <clarameniz@gmail.com>
2018-05-23 12:41:09 +0200

    Fix empty values for matrix in the chord widgets. (#86)

--- app/javascript/wizz-vis/utils/graph.js
+++ app/javascript/wizz-vis/utils/graph.js
@@ -1,4 +1,8 @@
 /* jshint esversion: 6 */
+
+import compact from 'lodash/compact';
+import flattenDeep from 'lodash/flattenDeep';
+
 export default {
   chord(data, dim_origin, dim_destination, aggregator) {
     let groups = Object.create(null),
@@ -6,34 +10,34 @@ export default {
         matrix = [], full_list;
 
     data.forEach(item => {
-      let dim0 = item[dim_origin]
-      let dim1 = item[dim_destination]
+      let dim0 = item[dim_origin];
+      let dim1 = item[dim_destination];
 
-      if (!groups[dim0]) groups[dim0] = {}
-      groups[dim0][dim1] = item[aggregator.name]
+      if (!groups[dim0]) groups[dim0] = {};
+      groups[dim0][dim1] = item[aggregator];
 
-      if(!from.includes(dim0)) from.push(dim0)
-      if(!to.includes(dim1)) to.push(dim1)
+      if(!from.includes(dim0)) from.push(dim0);
+      if(!to.includes(dim1)) to.push(dim1);
     });
 
-    full_list = from.concat(to.filter(d => !from.includes(d)))
+    full_list = from.concat(to.filter(d => !from.includes(d)));
 
     full_list.forEach(key => {
-      let row = groups[key]
-      let matrix_row = []
+      let row = groups[key];
+      let matrix_row = [];
       full_list.forEach(col => {
         matrix_row.push(
           Object.is(row, undefined) || Object.is(row[col], undefined) ? 0 : row[col]
-        )
-      })
-      matrix.push(matrix_row)
-    })
+        );
+      });
+      matrix.push(matrix_row);
+    });
 
     return {
-      value: matrix,
+      value: compact(flattenDeep(matrix)).length == 0 ? [] : matrix,
       keys: full_list.map((key) => {
-        return Object.is(key, null) ? 'N/A' : key
+        return Object.is(key, null) ? 'N/A' : key;
       })
     };
   },
-}
+};
