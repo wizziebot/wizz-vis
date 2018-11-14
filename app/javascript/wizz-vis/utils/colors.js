a3900e30da108c89f8668aead5f8f42709269d88
José Antonio Parra <josparbar@gmail.com>
2018-11-14 12:47:21 +0100

    WIZZIE-2876: extend the color palette used on graphs.

--- app/javascript/wizz-vis/utils/colors.js
+++ app/javascript/wizz-vis/utils/colors.js
@@ -2,7 +2,10 @@
 
 import {interpolateRgb, quantize} from "d3-interpolate";
 
-const COLORS = ['#3DCC91', '#FFB366', '#FF7373', '#FFCC00', '#3B22FF', '#8884d8'];
+const COLORS = ['#3DCC91', '#FFB366', '#FF7373', '#FFCC00', '#3B22FF',
+                '#8884d8', '#791E94', '#FFD07B', '#296EB4', '#B1740F',
+                '#2EC4B6', '#011627', '#BAD4AA', '#D4D4AA', '#84BCDA',
+                '#D6E681', '#BABF95', '#C4AD83', '#C6B677', '#FD5200'];
 
 export default {
   get(index) {
